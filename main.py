fd609311e0273a3beb70ccb256b374d86d3eb7e1
boshi <boshimoto@protonmail.com>
2022-01-05 15:12:41 -0500

    Rescoping previous function to return dict for future dev

--- main.py
+++ main.py
@@ -1,40 +1,32 @@
-import os
 from netmiko import ConnectHandler
-from time import localtime, strftime
+from time import strftime, localtime
 
-
-TIME = strftime('%m.%d.%I%M%p', localtime())
+TIME = strftime('%m.%d@%I;%M%p', localtime())
 
 
 def requestusername():
-    username = input('Please enter username: ')
+    username = input('Please enter username:')
     return username
 
 
 def requestpassword():
-    password = input('Please enter passphrase: ')
+    password = input('Please enter password:')
     return password
 
 
-def createdevicefile(username, password):
-    print('Type and enter each host name.\n\
-Type "exit" when finished.')
-    with open('devicenames', 'w') as devicefile:
-        while True:
-            host = input('Host name: ')
-            if host != 'exit':
-                newline = f'device_type: cisco_ios\n\
-host: {host}\n\
-username: {username}\n\
-password: {password}'
-                devicefile.write(newline + '\n')
-            else:
-                devicefile.close()
-                break
+def requesthostname():
+    host = input('Enter a host name:')
+    return host
 
 
-def deletedevicefile():
-    os.remove('devicenames')
+def createdevicedictionary(host, username, password):
+    device = {
+        'device_type': 'cisco_ios',
+        'host': host,
+        'username': username,
+        'password': password
+    }
+    return device
 
 
 # whid-co-dis-sw#
@@ -44,7 +36,7 @@ def deletedevicefile():
 # whid-fc-agg-r#
 
 
-def sshconnect(**device):
+def sshconnect(device):
     # make a connection
     connectdevice = ConnectHandler(**device)
     return connectdevice
@@ -69,23 +61,17 @@ commands = [
 ]
 
 
-username, password = requestusername(), requestpassword()
-createdevicefile(username, password)
-
-#  pending review  ########################
-connectdevice = sshconnect(**device)
+username, password, host = requestusername(), requestpassword(), requesthostname()
+device = createdevicedictionary(host, username, password)
+connectdevice = sshconnect(device)
 runcommands(commands, connectdevice)
-disconnectdevice = sshdisconnect(connectdevice)
-##########################################
-
-deletedevicefile()
+sshdisconnect(connectdevice)
 
 
+#######################################################################
 # make a directory - os.mkdir()? to upload multiple files?
 # learn/apply encryption for the file? use hash?
 # additional devices?
-
-#######################################################################
 
 # config.txt
 # mode=development
