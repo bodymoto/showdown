daf72306ed449bd4eed037bb34c67758a74daceb
boshi <boshimoto@protonmail.com>
2022-01-07 13:57:22 -0500

    Reorganized main to accept multiple host inputs/outputs

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import definitions as do
 
-host, username, password = do.requesthost(), do.requestusername(), do.requestpassword()
-device = do.createdevicedictionary(host, username, password)
-connectdevice = do.sshconnect(device)
-commands = do.mycommands()  # input device
-do.createdirectory()
-do.runcommands(host, connectdevice, commands)
-do.sshdisconnect(connectdevice)
+
+def staticdata():
+    username, password = do.requestusername(), do.requestpassword()
+    do.createdirectory()
+    return username, password
+
+
+def dynamicdata(username, password):
+    host = do.requesthost()
+    device = do.createdevicedictionary(host, username, password)
+    connectdevice = do.sshconnect(device)
+    commands = do.mycommands()  # input device
+    do.runcommands(host, connectdevice, commands)
+    do.sshdisconnect(connectdevice)
+
+
+def repeatdynamicdata(username, password):
+    while True:
+        print('Any additional hosts?')
+        confirm = input('Enter "y" to continue: ')
+        if confirm == 'y':
+            dynamicdata(username, password)
+        else:
+            break
+
+
+def main():
+    username, password = staticdata()
+    dynamicdata(username, password)
+    repeatdynamicdata(username, password)
+
+
+main()
